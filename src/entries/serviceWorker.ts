e852b94a381f1afeb0b8ab8c48197dc3217bbc04
Matthew <morch028@uottawa.ca>
2022-12-29 17:16:46 -0500

    Extract helper

--- src/entries/serviceWorker.ts
+++ src/entries/serviceWorker.ts
@@ -26,29 +26,41 @@ function getSelectedText() {
   return window.getSelection()?.toString() ?? "";
 }
 
+const getManualSelectionText = async (
+  tabId: number | null,
+  frameId: number | null
+) => {
+  if (!tabId) return null;
+
+  const hasScriptAccess = await ScriptingManager.hasAccess();
+  if (!hasScriptAccess) return null;
+
+  try {
+    return await ScriptingManager.executeScript(
+      { tabId, frameId },
+      getSelectedText
+    );
+  } catch (error) {
+    console.warn("Unable to grab multiline input", error);
+    return null;
+  }
+};
+
 chrome.contextMenus.onClicked.addListener(
   async (baseContextMenuInfo, sourceTab) => {
     console.log("Context menu clicked", { baseContextMenuInfo, sourceTab });
 
-    let manualSelectionText = null;
-    if (sourceTab?.id && (await ScriptingManager.hasAccess())) {
-      try {
-        manualSelectionText = await ScriptingManager.executeScript(
-          { tabId: sourceTab.id, frameId: baseContextMenuInfo.frameId ?? null },
-          getSelectedText
-        );
-      } catch (error) {
-        console.warn("Unable to grab multiline input", error);
-      }
-    }
-
     const targetId = createId();
-    const contextMenuInfo = { ...baseContextMenuInfo, manualSelectionText };
-
     await mailboxRepository.upsertDrop({
-      contextMenuInfo,
-      sourceTabId: sourceTab?.id ?? null,
       targetId,
+      sourceTabId: sourceTab?.id ?? null,
+      contextMenuInfo: {
+        ...baseContextMenuInfo,
+        manualSelectionText: await getManualSelectionText(
+          sourceTab?.id ?? null,
+          baseContextMenuInfo.frameId ?? null
+        ),
+      },
     });
     await chrome.tabs.create({
       url: LinkHrefs.snippetTarget(targetId),
