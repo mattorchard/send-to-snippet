dceb4dd825e21b0958100df97f06f54d09056406
Matthew <morch028@uottawa.ca>
2022-12-29 15:13:00 -0500

    Makes error wrapping preserve more context

--- src/helpers/errorHelpers.ts
+++ src/helpers/errorHelpers.ts
@@ -1,5 +1,16 @@
 export const wrapError = (error: unknown) => {
   if (error instanceof Error) return error;
-  if (typeof error === "string") return new Error(error);
-  return new Error(`Unknown error "${error}"`);
+  const message = extractMessage(error);
+  if (message) return new Error(message, { cause: error });
+  return new Error(`Unknown error "${error}"`, { cause: error });
+};
+
+const extractMessage = (error: unknown): string | null => {
+  if (typeof error === "string") return error;
+  if (error === null) return `Received null error`;
+  if (error === undefined) return `Received undefined error`;
+  // @ts-ignore
+  if (error && typeof error.message === "string") return error.message;
+
+  return null;
 };
