072429ec5a795415a92ecb32363f9142c88c4e47
Matthew <morch028@uottawa.ca>
2022-12-29 15:13:43 -0500

    Fixes wrapping call

--- src/helpers/ScriptingManager.ts
+++ src/helpers/ScriptingManager.ts
@@ -6,8 +6,7 @@ export const ScriptingManager = {
   askUserToGrantAccess: () =>
     extension.askUserToGrantPermissions({ permissions: scriptingPermissions }),
 
-  hasAccess: () =>
-    extension.askUserToGrantPermissions({ permissions: scriptingPermissions }),
+  hasAccess: () => extension.hasAccess({ permissions: scriptingPermissions }),
 
   executeScript: async <T>(tabId: number, func: () => T) =>
     extension.executeScript(tabId, func),
