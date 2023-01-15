957eed0068fd70a9ec7fc3ef1c79605a93d4938b
Matt Orchard <morch028@uottawa.ca>
2023-01-15 16:48:14 -0500

    Fix error alert styling

--- src/components/ErrorAlert.tsx
+++ src/components/ErrorAlert.tsx
@@ -2,7 +2,7 @@ import { h, FunctionComponent, Fragment } from "preact";
 import { Alert } from "./Alert";
 
 export const ErrorAlert: FunctionComponent<{ error: Error }> = ({ error }) => {
-  const stackCount = error.stack?.split("\n").length ?? 0;
+  const stackCount = error.stack?.split("\n", 10).length ?? 0;
   return (
     <Alert
       intent="error"
@@ -10,7 +10,7 @@ export const ErrorAlert: FunctionComponent<{ error: Error }> = ({ error }) => {
       body={
         <Fragment>
           <p>{error.message}</p>
-          <code style={{ height: `${Math.min(stackCount, 10)}rem` }}>
+          <code style={{ height: `${stackCount}rem` }}>
             <pre>{error.stack}</pre>
           </code>
         </Fragment>
