5d9185d679a8e413dd6a1cb72fd4935a07062c43
Matt Orchard <morch028@uottawa.ca>
2023-01-15 17:25:55 -0500

    Keep single listener with rotating key

--- src/hooks/useSandboxReply.ts
+++ src/hooks/useSandboxReply.ts
@@ -22,49 +22,48 @@ const initialReplyState: SandboxReplyState = {
 const logger = new Logger("SandboxReply");
 
 export const useSandboxReply = (key: string) => {
-  const ref = useRef<HTMLIFrameElement>(undefined!);
+  const iframeRef = useRef<HTMLIFrameElement>(undefined!);
   const [state, setState] = useState(initialReplyState);
+  const keyRef = useRef(key);
 
   useEffect(() => {
     setState(initialReplyState);
+    keyRef.current = key;
   }, [key]);
 
   useWindowEvent(
     "message",
-    useCallback(
-      (event: MessageEvent<FromSandboxMessage>) => {
-        if (event.source !== ref.current.contentWindow) return;
-        const { data } = event;
-        if (data.runId !== key) return;
-        logger.debug("Main thread received reply", data);
-        switch (data.kind) {
-          case "dom-mutation":
-            setState((state) => ({ ...state, hasMutated: true }));
-            break;
-          case "resolve":
-            setState((state) => ({
-              ...state,
-              hasResolved: true,
-              result: data.result,
-            }));
-            break;
-          case "error":
-            setState((state) => ({
-              ...state,
-              hasError: true,
-              error: data.error,
-            }));
-            break;
-          default:
-            logger.warn("Received unexpected message kind", data);
-        }
-      },
-      [key]
-    )
+    useCallback((event: MessageEvent<FromSandboxMessage>) => {
+      if (event.source !== iframeRef.current.contentWindow) return;
+      const { data } = event;
+      if (data.runId !== keyRef.current) return;
+      logger.debug("Main thread received reply", data);
+      switch (data.kind) {
+        case "dom-mutation":
+          setState((state) => ({ ...state, hasMutated: true }));
+          break;
+        case "resolve":
+          setState((state) => ({
+            ...state,
+            hasResolved: true,
+            result: data.result,
+          }));
+          break;
+        case "error":
+          setState((state) => ({
+            ...state,
+            hasError: true,
+            error: data.error,
+          }));
+          break;
+        default:
+          logger.warn("Received unexpected message kind", data);
+      }
+    }, [])
   );
 
   return {
-    ref,
+    ref: iframeRef,
     state,
     isLoading: !state.hasMutated && !state.hasError && !state.hasResolved,
   };
